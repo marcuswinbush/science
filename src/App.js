76ddf9b3d45fd11685f359aedaed8aa39e024174
Marcus Winbush <mwinbush00@gmail.com>
2024-07-08 17:52:38 -0400

    creating navbar

--- src/App.js
+++ src/App.js
@@ -3,7 +3,31 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="bg-red-300">
+      <header className='py-5 px-25 top-0 left-0 fixed flex justify-between items-center w-full '>
+        <a className='logo'>
+        Elemental Analytics
+        </a>
+        <nav>
+        <a className='' href="/">
+          Home
+        </a>
+
+        <a className='/about'>
+          About
+        </a>
+
+        <a className='/careers'>
+          Careers
+          
+        </a>
+        <a className='/industries'>
+          Industries
+        </a>
+
+        </nav>
+        
+      </header>
      
     </div>
   );
