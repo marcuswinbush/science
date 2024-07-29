b3dd6c676ff8c497296cacd68a6974d021c16a4b
Marcus Winbush <mwinbush00@gmail.com>
2024-07-29 17:12:14 -0400

    adjusting images

--- src/Components/Main/main.js
+++ src/Components/Main/main.js
@@ -5,24 +5,31 @@ import images from '../../Assets/images'
 const Main = () => {
 
 return(
-<section className="w-full relative m-auto justify-center pt-8 items-center">
+<section className="w-full relative m-auto justify-center pt-4 items-center">
     
     <div className=" h-96 w-full top-0 bottom-0 overflow-hidden">
     <video  src={video} muted autoPlay loop type="science/mp4"></video>
    </div>
    
   <div className="p-5">
-  <h1>What we Offer</h1>
-  <div className="w-3/4 flex">
+  <h1> What We Offer</h1>
+  <motion.div className=" flex  h-full">
   {images.map(image => {
  return(
-    <div>
-      <img src={image}/>
-    </div>
-    );
+    <motion.div 
+    whileHover={{ scale: 1.1, }}
+    
+    className="flex m-2 overflow-hidden  ">
+      <img className="cursor-pointer rounded-md" alt="science"
+      src={image}/>
+    </motion.div>
+    ); 
   })}
 
+  </motion.div>
   </div>
+  <div>
+    <h1>About Us</h1>
   </div>
 
 
